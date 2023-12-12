d19be3cbd120d1f717b434211cd334169f3a469e
ArnoDeZwart9 <151937445+ArnoDeZwart9@users.noreply.github.com>
2023-12-12 19:29:16 +0100

    Full HD environment

--- highway/envy_test.py
+++ highway/envy_test.py
@@ -3,6 +3,18 @@ import highway_env as env
 from matplotlib import pyplot as plt
 
 env = gym.make('merge-v0', render_mode='rgb_array')
+
+env.configure({
+    "screen_width": 1920,
+    "screen_height": 1080,
+    "scaling": 10,
+    "renderfps": 15
+})
+env.configure({
+    "show_trajectories": False,
+    "manual_control": True
+})
+
 env.reset()
 for _ in range(300):
     action = env.action_type.actions_indexes["IDLE"]
