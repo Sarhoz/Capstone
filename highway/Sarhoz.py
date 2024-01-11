677189b7bd1f2ef2e0ea23d6904d0b5726689934
Sarhoz Mahmoud <sozan1@live.nl>
2024-01-11 15:55:55 +0100

    Addition of Extra model
    Added multiple models that are needed

--- highway/Sarhoz.py
+++ highway/Sarhoz.py
@@ -4,27 +4,63 @@ from matplotlib import pyplot as plt
 import pprint
 from stable_baselines3 import DQN
 import cv2
+from stable_baselines3 import PPO
+from sb3_contrib import TRPO
 
 # Video
 frameSize = (1280,560)
 out = cv2.VideoWriter('video'+"-Merging"+'.avi', cv2.VideoWriter_fourcc(*'mp4v'), 16, frameSize)
 
-def DQN_Creation():
+def model_creation(model_name: str):
     env = gym.make("merge-in-v0")
-    model = DQN('MlpPolicy', env,
-              policy_kwargs=dict(net_arch=[256, 256]),
-              learning_rate=5e-4,
-              buffer_size=15000,
-              learning_starts=200,
-              batch_size=32,
-              gamma=0.8,
-              train_freq=1,
-              gradient_steps=1,
-              target_update_interval=50,
-              verbose=1,
-              tensorboard_log="highway_dqn/")
-    model.learn(30000)
-    model.save("highway_dqn/model")
+    if (model_name == "DQN"):
+        model = DQN('MlpPolicy', env,
+                policy_kwargs=dict(net_arch=[256, 256]),
+                learning_rate=5e-4,
+                buffer_size=15000,
+                learning_starts=200,
+                batch_size=32,
+                gamma=0.8,
+                train_freq=1,
+                gradient_steps=1,
+                target_update_interval=50,
+                verbose=1,
+                tensorboard_log="highway_dqn/")
+        model.learn(30000)
+        model.save("highway_dqn/model")
+    elif (model_name == "PPO"):
+        model = PPO("MlpPolicy", env,
+                    learning_rate=0.0003)
+        model.learn(30000)
+        model.save("highway_ppo/model")
+    elif (model_name == ""):
+        model = model = TRPO("MlpPolicy", env,
+             learning_rate=0.0003,
+             n_steps=1024,
+             batch_size=128,
+             gamma=0.99,
+             cg_max_steps=15,
+             cg_damping=0.1,
+             line_search_shrinking_factor=0.8,
+             line_search_max_iter=10,
+             n_critic_updates=10,
+             gae_lambda=0.95,
+             use_sde=False,
+             sde_sample_freq=-1,
+             normalize_advantage=True,
+             target_kl=0.015,
+             sub_sampling_factor=1,
+             policy_kwargs=None,
+             verbose=1,
+             tensorboard_log="highway_TRPO/",
+             seed=None,
+             device='cuda',
+             _init_setup_model=True)
+        model.learn(30000)
+        model.save("highway_trpo/model")
+    else:
+        print("Input model does not exist!")
+
 
 # Unlearned Agent
 def Unlearned_Agent():
@@ -56,14 +92,15 @@ def Unlearned_Agent():
         out.write(cur_frame)
     out.release()
 
-def DQN_Agent():
-
-
+def DRL_Models():
+    
     # Train model 
-    #DQN_Creation()
+    # model_creation("DQN")
 
     # Load model
     model = DQN.load("highway_dqn/model")
+    # model = PPO.load("highway_ppo/model")
+    # model = TPRO.load("highway_trpo/model")
 
     env = gym.make('merge-in-v0', render_mode='rgb_array')
     env.configure({
@@ -73,15 +110,26 @@ def DQN_Agent():
     })
 
     # create a image
-    done = truncated = False
-    obs, info = env.reset()
+    number_of_collisions = 0
+    T = 0
+    while T <= 100:
     
-    while not (done or truncated):
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, truncated, info = env.step(action)
-        env.render()
-        cur_frame = env.render()
-        out.write(cur_frame)
+        done = truncated = False
+        obs, info = env.reset()
+    
+        while not (done or truncated):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, truncated, info = env.step(action)
+            #print(info)
+            if info.get('crashed'):
+                number_of_collisions += 1
+            env.render()
+            cur_frame = env.render()
+            out.write(cur_frame)
+        T+=1
+    print('crashrate is '+str(float(number_of_collisions)/T)+' and T is'+ str(T))
+    print('number_of_collisions is:', number_of_collisions)
+    print('DONE')
     out.release()
 
-DQN_Agent()
+DRL_Models()
