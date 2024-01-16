f7ff304477841d3679d20c490dc48cf7ff30007b
SalBoza <salih.bozaci38@gmail.com>
2024-01-16 15:46:30 +0100

    Added environmental behaviour to merge_in_v3

--- highway_env_copy/envs/merge_in_env.py
+++ highway_env_copy/envs/merge_in_env.py
@@ -315,7 +315,7 @@ class MergeinEnvSalih(MergeinEnv):
             + right_lane_reward
         )
 
-        if self.is_terminated() and not self.vehicle.crashed:
+        if self._is_terminated() and not self.vehicle.crashed:
             reward += 1
         
         return utils.lmap(reward, [0, 1], [self.config["collision_penalty"] + self.config["merging_speed_penalty"], 1])
@@ -392,4 +392,86 @@ class MergeinEnvSalih(MergeinEnv):
         scaled_speed = utils.lmap(self.vehicle.speed, speed_range, [0, 1])
         return scaled_speed
 
+    def _is_terminated(self) -> bool:
+        """The episode is over when a collision occurs or when the access ramp has been passed."""
+        # print("crash" + str(self.vehicle.crashed))
+        # print("over"  + str(self.vehicle.position[0] > 370))
+        return self.vehicle.crashed or bool(self.vehicle.position[0] > 370)
 
+
+    def _is_truncated(self) -> bool:
+        return False
+
+    def _reset(self) -> None:
+        self._make_road()
+        self._make_vehicles()
+
+    def _make_road(self) -> None:
+        """
+        Make a road composed of a straight highway and a merging lane.
+
+        :return: the road
+        """
+        net = RoadNetwork()
+
+        # Highway lanes
+        ends = [150, 80, 80, 150]  # Before, converging, merge, after
+        c, s, n = LineType.CONTINUOUS_LINE, LineType.STRIPED, LineType.NONE
+        y = [0, StraightLane.DEFAULT_WIDTH,2*StraightLane.DEFAULT_WIDTH]
+        line_type = [[c, s],[n, s], [n, c]]
+        line_type_merge = [[c, s],[n, s], [n, s]]
+        for i in range(3):
+            net.add_lane("a", "b", StraightLane([0, y[i]], [sum(ends[:2]), y[i]], line_types=line_type[i]))
+            net.add_lane("b", "c", StraightLane([sum(ends[:2]), y[i]], [sum(ends[:3]), y[i]], line_types=line_type_merge[i]))
+            net.add_lane("c", "d", StraightLane([sum(ends[:3]), y[i]], [sum(ends), y[i]], line_types=line_type[i]))
+
+        # Merging lane
+        amplitude = 3.25
+        ljk = StraightLane([0, 6.5 + 4 + 4+ 4], [ends[0], 6.5 + 4 + 4+ 4], line_types=[c, c], forbidden=True)
+        lkb = SineLane(ljk.position(ends[0], -amplitude), ljk.position(sum(ends[:2]), -amplitude),
+                       amplitude, 2 * np.pi / (2*ends[1]), np.pi / 2, line_types=[c, c], forbidden=True)
+        lbc = StraightLane(lkb.position(ends[1], 0), lkb.position(ends[1], 0) + [ends[2], 0],
+                           line_types=[n, c])
+        net.add_lane("j", "k", ljk)
+        net.add_lane("k", "b", lkb)
+        net.add_lane("b", "c", lbc)
+        road = Road(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
+        road.objects.append(Obstacle(road, lbc.position(ends[2], 0)))
+        self.road = road
+
+    def _make_vehicles(self) -> None:
+        """
+        Populate a road with several vehicles on the highway and on the merging lane, as well as an ego-vehicle.
+
+        :return: the ego-vehicle
+        """
+        road = self.road
+        ego_vehicle = self.action_type.vehicle_class(road,
+                                                     road.network.get_lane(("j", "k", 0)).position(30, 0),
+                                                     speed=25)
+        road.vehicles.append(ego_vehicle)
+
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+
+
+        # Generate a list of 20 tuples with random position and speed values
+        number_of_cars = 10
+        random_cars = [(self.np_random.uniform(0, 1), self.np_random.uniform(27, 30)) for _ in range(number_of_cars)]
+
+
+        for i , (position, speed) in enumerate(random_cars):
+            lane = road.network.get_lane(("a", "b", self.np_random.integers(2)))
+            position = lane.position(position + (380/number_of_cars)* i , 0)#+ self.np_random.uniform(-5, 5)
+            speed = speed #+= self.np_random.uniform(-1, 1)
+            road.vehicles.append(other_vehicles_type(road, position, speed=speed))
+        
+        # Add a vehicle before and after the ego vehicle on the merging lane
+
+        # merge_lane = road.network.get_lane(("b", "c", 0))
+        # road.vehicles.append(other_vehicles_type(road, merge_lane.position(10, 0), 25))
+        # road.vehicles.append(other_vehicles_type(road, merge_lane.position(100, 0), 25))
+
+        # merging_v = other_vehicles_type(road, road.network.get_lane(("j", "k", 0)).position(110, 0), speed=20)
+        ego_vehicle.target_speed = 30
+        # road.vehicles.append(merging_v)
+        self.vehicle = ego_vehicle
