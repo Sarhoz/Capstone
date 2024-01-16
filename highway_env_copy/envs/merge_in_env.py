fix ttc computation in merge_in_v3
TTC computation didnt work, so added calculation from MergeInEnvSarhoz

highway_env_copy/envs/merge_in_env.py
@@ -293,7 +293,8 @@ class MergeinEnvSalih(MergeinEnv):
                 "reward_speed_range": [20, 30],
                 "merging_speed_penalty": -0.5,
                 "lane_change_penalty": -0.05,
-                "ttc_reward_weight": 3 # kan wss weg als we niet gebruiken,
+                "ttc_reward_weight": 3,
+                "other_vehicles": 9
         })
         return cfg
 
@@ -320,12 +321,7 @@ class MergeinEnvSalih(MergeinEnv):
         return utils.lmap(reward, [0, 1], [self.config["collision_penalty"] + self.config["merging_speed_penalty"], 1])
     
     def _rewards(self, action):
-        ttc_reward, ttc = self._compute_ttc()
-        if ttc < 3:
-            ttc_reward = 1 - 3 / ttc
-        else:
-            ttc_reward = 0
-
+        ttc_reward = self._compute_ttc()
         return {
             "ttc_reward": self.config["ttc_reward_weight"] * ttc_reward,
             "collision_penalty": self.vehicle.crashed,
@@ -355,6 +351,41 @@ class MergeinEnvSalih(MergeinEnv):
                 return time_to_collision
             else:
                 return float("inf")
+
+    def _compute_ttc(self):
+        TTC = None
+        obs_matrix = KinematicObservation(self, absolute=False, vehicles_count=self.config["other_vehicles"],
+                                         normalize=False).observe()
+        use_TTC = False
+        glob_TTC = float('inf')
+        for vehicle in range(1, len(obs_matrix)):
+            x_pos = obs_matrix[vehicle][1]
+            y_pos = -1 * obs_matrix[vehicle][2]
+            pos_vec = [x_pos, y_pos]  # this is relative when absolute = False
+            vx = obs_matrix[vehicle][3]
+            vy = -1 * obs_matrix[vehicle][4]
+            vel_vec = [vx, vy]
+            if np.dot(pos_vec, pos_vec) != 0:
+                proj_pos_vel = np.multiply(np.dot(vel_vec, pos_vec) / np.dot(pos_vec, pos_vec), pos_vec)
+                len_pos = np.linalg.norm(pos_vec)
+                len_proj = np.linalg.norm(proj_pos_vel)
+
+                if proj_pos_vel[0] * vel_vec[0] > 0 and proj_pos_vel[1] * vel_vec[1] > 0:  # collinear so TTC infinite
+                    TTC = float('Inf')
+                else:
+                    TTC = len_pos / len_proj
+            else:
+                TTC = float('Inf')
+            if TTC > 0:  # just to be safe
+                glob_TTC = min(glob_TTC, TTC)
+
+        if glob_TTC < 3:  # only care about TTC if crash is close
+            ttc_reward = 1 - 3 / glob_TTC
+        else:
+            ttc_reward = 0
+
+        return ttc_reward
+    
 
     def _compute_high_speed_reward(self) -> float:
         speed_range = self.config["reward_speed_range"]