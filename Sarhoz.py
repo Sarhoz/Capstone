ec40bab3a2e7d1c7368f96dc212617692c5e3d06
Sarhoz Mahmoud <sozan1@live.nl>
2024-01-17 17:16:51 +0100

    small changes
    functions made easier

--- Sarhoz.py
+++ Sarhoz.py
@@ -8,175 +8,18 @@ import cv2
 from stable_baselines3 import PPO
 from sb3_contrib import TRPO
 import numpy as np
-
 from highway_env_copy.vehicle.kinematics import Performance, Logger
 
-
-# Video
-frameSize = (1280,560)
-out = cv2.VideoWriter('video'+"-Merging"+'.avi', cv2.VideoWriter_fourcc(*'mp4v'), 16, frameSize)
-
-# Create enviromenent
-env = gym.make("merge-in-v3", render_mode = "rgb_array")
-#env = gym.make("racetrack-v0", render_mode = "rgb_array")
-
-# env.configure({
-#      "action": {
-#             "type": "ContinuousAction"
-#             },
-#     })
-
-def model_creation(model_name: str):
-    
-    env.reset()
-    pprint.pprint(env.config)
-
-    if (model_name == "DQN"):
-        print("DQN")
-        # DQN CAN NOT LEARN CONTINUOUS!!!
-        model = DQN('MlpPolicy', env,
-              policy_kwargs=dict(net_arch=[256, 256]),
-              learning_rate=5e-4,
-              buffer_size=15000,
-              learning_starts=200,
-              batch_size=32,
-              gamma=0.8,
-              train_freq=1,
-              gradient_steps=1,
-              target_update_interval=50,
-              verbose=1,
-              #tensorboard_log="highway_DQN/",
-              device='cuda')
-        model.learn(20000)
-        model.save("highway_dqn/model")
-    elif (model_name == "PPO"):
-        print("PPO")
-        model = PPO("MlpPolicy", env,
-                verbose=1,
-                learning_rate=0.0001,
-                batch_size=32,
-                gamma=0.8,
-                #tensorboard_log="highway_PPO/",
-                device='cuda')
-        model.learn(150000)
-        model.save("highway_ppo/model-S1")
-    elif (model_name == "TRPO"):
-        print("TRPO")
-        model = TRPO("MlpPolicy", env,
-             learning_rate=0.0003,
-             n_steps=1024,
-             batch_size=128,
-             gamma=0.99,
-             cg_max_steps=15,
-             cg_damping=0.1,
-             line_search_shrinking_factor=0.8,
-             line_search_max_iter=10,
-             n_critic_updates=10,
-             gae_lambda=0.95,
-             use_sde=False,
-             sde_sample_freq=-1,
-             normalize_advantage=True,
-             target_kl=0.015,
-             sub_sampling_factor=1,
-             policy_kwargs=None,
-             verbose=1,
-             #tensorboard_log="highway_TRPO/",
-             seed=None,
-             device='cuda',
-             _init_setup_model=True)
-        model.learn(150000)
-        model.save("highway_trpo/model-cont-S1")
-    else:
-        print("Input model does not exist!")
+from function import environment, baseline_models, performance_model
 
 
-def DRL_Models():
-    
-    # Train model 
+# Create Environment
+env = environment("merge-in-v0")
 
-    #model_creation("DQN")
-    #model_creation("PPO")
-    #model_creation("TRPO")
+# Creating the Baseline Models
+#baseline_models("DQN", env, 80000, False)
+#baseline_models("PPO", env, 50000, False)
+#baseline_models("TRPO", env, 50000, False)
 
-    # Load model
-    model = DQN.load("highway_dqn/model")
-    #model = PPO.load("highway_ppo/model-S1") 
-    #model = TRPO.load("highway_trpo/model-cont-S1")
-
-    env.configure({
-    "screen_width": 1280,
-    "screen_height": 560,
-    "renderfps": 16
-    })
-
-    pprint.pprint(env.config)
-
-    #Performance and logger
-    perfm = Performance()
-    lolly = Logger()
-
-    
-    # create a image
-    number_of_collisions = 0
-    T = 1
-    best_reward = -float('inf') # initialize the best reward with negative infinity
-    rewards = [] #initialize list of rewards
-    while T <= 100:
-    
-        done = truncated = False
-        obs, info = env.reset()
-        reward = 0
-        ego_car = env.controlled_vehicles[0]
-        stepcounter = 0
-
-        total_reward = 0 # total reward for this epoch
-
-        while not (done or truncated):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, truncated, info = env.step(action)
-            stepcounter += 1
-            total_reward += reward
-
-            lolly.file(ego_car)
-
-            if info.get('crashed'):
-                number_of_collisions += 1
-            env.render()
-            cur_frame = env.render()
-            out.write(cur_frame)
-
-        if total_reward > best_reward:
-            best_reward = total_reward
-        rewards.append(total_reward)
-        T+=1
-        perfm.add_measurement(lolly)
-        lolly.clear_log()
-        print(T)
-
-
-    plt.plot(rewards)
-    plt.title("Rewards per run")
-    plt.xlabel('Runs')
-    plt.ylabel("Total Reward")
-    plt.show()
-
-    perfm.print_performance()
-    
-    print(f'Best Reward: {best_reward}') # print best reward
-
-    print('crashrate is '+str(float(number_of_collisions)/T)+' and T is'+ str(T))
-    print('number_of_collisions is:', number_of_collisions)
-    
-    # a stand for "append"
-    with open("Performance.txt", "a") as file:
-        file.write(f"\n The TRPO with base rewards (no Tuning and ContiniousAction) -- 100 runs -- merge \n \n")
-        file.write(f"{perfm.string_rep()}")
-        file.write(f"\n")
-        file.write(f"{perfm.array_rep()}")
-        file.write(f"\n\n")
-
-    print('DONE')
-    out.release()
-
-DRL_Models()
-
+# Look at the Performance
+performance = performance_model(env, PPO, "highway_ppo/model-baseline", 50, "baseline_test", i=2)
---
+++ function.py
@@ -0,0 +1,141 @@
+import gymnasium as gym
+from stable_baselines3 import DQN
+from stable_baselines3 import PPO
+from sb3_contrib import TRPO
+from highway_env_copy.vehicle.kinematics import Performance, Logger
+from matplotlib import pyplot as plt
+import cv2
+
+# Created environment
+def environment(environment_name: str):
+    env = gym.make(environment_name, render_mode = "rgb_array")
+    
+    env.configure({
+    "screen_width": 1280,
+    "screen_height": 560,
+    "renderfps": 16})
+
+    env.reset()
+    
+    return env
+
+
+
+# Training/loading baseline models
+def baseline_models(model: str, env, iterations: int, rewards: bool):
+    if model.upper() == "TRPO":
+        model = TRPO("MlpPolicy", env,
+                     tensorboard_log="Tensorboard_log/baseline_TRPO",
+                     device="cuda",
+                     verbose=1)
+        model.learn(iterations)
+        print(f"{model} has finished training with {iterations} iterations!")
+        if rewards:
+            model.save("highway_trpo/model-baseline-rewards")
+        else:
+            model.save("highway_trpo/model-baseline")
+        return model
+    elif model.upper() == "PPO":
+        model = PPO("MlpPolicy", env,
+                     tensorboard_log="Tensorboard_log/baseline_PPO",
+                     device="cuda",
+                     verbose=1)
+        model.learn(iterations)
+        print(f"{model} has finished training with {iterations} iterations!")
+        if rewards:
+            model.save("highway_ppo/model-baseline-rewards")
+        else:
+            model.save("highway_ppo/model-baseline")
+        return model
+    elif model.upper() == "DQN":
+        model = DQN("MlpPolicy", env,
+                     tensorboard_log="Tensorboard_log/baseline_DQN",
+                     device="cuda",
+                     verbose=1)
+        model.learn(iterations)
+        print(f"{model} has finished training with {iterations} iterations!")
+        if rewards:
+            model.save("highway_dqn/model-baseline-rewards")
+        else:
+            model.save("highway_dqn/model-baseline")
+        return model
+    else:
+        print("The input algorithm does not exist!")
+
+# Check the Performance of a model
+def performance_model(env, model, model_path: str, number_of_tests: int, video_name:str, i: int):
+    
+    # Video
+    frameSize = (1280,560)
+    out = cv2.VideoWriter('video'+"-Merging"+ video_name + '.avi', cv2.VideoWriter_fourcc(*'mp4v'), 16, frameSize)
+
+    # load model
+    model = model.load(model_path)
+
+    # Performance and logger
+    perfm = Performance()
+    lolly = Logger()
+
+    # Test run
+    number_of_collisions = 0
+    T = 0
+    best_reward = -float('inf') # initialize the best reward with negative infinity
+    rewards = [] #initialize list of rewards
+    for i in range(number_of_tests):
+    
+        done = truncated = False
+        obs, info = env.reset()
+        reward = 0
+
+        ego_car = env.controlled_vehicles[0]
+
+        total_reward = 0 # total reward for this epoch
+
+        while not (done or truncated):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, truncated, info = env.step(action)
+            total_reward += reward
+
+            lolly.file(ego_car)
+
+            if info.get('crashed'):
+                number_of_collisions += 1
+
+            env.render()
+            cur_frame = env.render()
+            out.write(cur_frame)
+
+        if total_reward > best_reward:
+            best_reward = total_reward
+
+        rewards.append(total_reward)
+        T+=1
+        perfm.add_measurement(lolly)
+        lolly.clear_log()
+
+    plt.plot(rewards)
+    plt.title("Rewards per run")
+    plt.xlabel('Runs')
+    plt.ylabel("Total Reward")
+    plt.show()
+
+    perfm.print_performance()
+    
+    print(f'Best Reward: {best_reward}') # print best reward
+    print('crashrate is '+str(float(number_of_collisions)/T)+' and T is'+ str(T))
+    print('number_of_collisions is:', number_of_collisions)
+    
+    writing = "w" if i == 0 else "a"
+    with open("Performance.txt", writing) as file:
+        file.write(f"\n The TRPO with base rewards (no Tuning and ContiniousAction) -- 100 runs -- merge \n \n")
+        file.write(f"{perfm.string_rep()}")
+        file.write(f"\n")
+        file.write(f"{perfm.array_rep()}")
+        file.write(f"\n\n")
+
+    print('DONE')
+    out.release()
+
+    return perfm.array_rep()
+
+
