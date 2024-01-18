cafcd6d401ff257ef65551d7d1034e1467157789
Sarhoz Mahmoud <sozan1@live.nl>
2024-01-18 23:22:51 +0100

    Baseline models done

--- Sarhoz.py
+++ Sarhoz.py
@@ -14,7 +14,7 @@ env = environment("merge-in-v0", False)
 # Creating the Baseline Models
 #baseline_models("DQN", env, 80000, False)
 #baseline_models("PPO", env, 50000, False)
-baseline_models("TRPO", env, 50000, False)
+#baseline_models("TRPO", env, 50000, False)
 
 # Look at the Performance
 performance = performance_model(env=env, model=TRPO, model_path="highway_TRPO\model-baseline.zip", model_name="TRPO",
